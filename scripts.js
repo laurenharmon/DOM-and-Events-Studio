01a41db47fc2043cf6c06ef75cdf7f430e2b3d6c
Lauren Harmon <harmon.lauren@hotmail.com>
2020-10-14 18:11:37 -0500

    3rd requirement met

--- scripts.js
+++ scripts.js
@@ -6,11 +6,17 @@ window.addEventListener("load", function() {
     let popUpResult = window.confirm("Confirm that the shuttle is ready for takeoff.");
         if (popUpResult === true) {
             flightStatus.innerHTML = "Shuttle in flight!";
-            shuttleBackground.style.backgroundColor = "#668B8B";  
+            shuttleBackground.style.backgroundColor = "blue";  
             spaceShuttleHeight.innerHTML = "10,000";
         }
 
-
-
+    let landButton = document.getElementById("landing");
+        landButton.addEventListener("click", function (event) {
+            window.alert("The shuttle is landing. Landing gear engaged.");
+            flightStatus.innerHTML = "The shuttle has landed.";
+            shuttleBackground.style.backgroundColor = "green";
+            spaceShuttleHeight.innerHTML = "0";
+    });
+    
 
 });
