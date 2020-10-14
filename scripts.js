1ace01490c8df8ae0264670c9098915cd04185a7
Lauren Harmon <harmon.lauren@hotmail.com>
2020-10-14 18:05:44 -0500

    initial commit, first and second requirements met

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+window.addEventListener("load", function() {
+
+    let popUpResult = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (popUpResult === true) {
+            flightStatus.innerHTML = "Shuttle in flight!";
+            shuttleBackground.style.backgroundColor = "#668B8B";  
+            spaceShuttleHeight.innerHTML = "10,000";
+        }
+
+
+
+
+});
